83e0da256b2426acb3790036871b03eccbd8b134
Caio Hodos <caio.hodos@yahoo.com.br>
2026-05-20 18:16:19 -0300

    Inclui reproducao de som e controles do mesmo

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const musicaBt = document.querySelector('#alternar-musica');
+const musica = new Audio('sons/luna-rise-part-one.mp3');
+
+musica.loop = true;
+musicaBt.addEventListener('change', () => {
+    if(musica.paused){
+        musica.play();
+    }
+    else {
+        musica.pause();
+    }
+});
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco');
